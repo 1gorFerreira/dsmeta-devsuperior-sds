bf15d512796b79ff63785bfa5cf6369423f90414
Igor <igorferreiradaniel99@hotmail.com>
2022-09-11 00:23:02 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +20,30 @@ public class SaleService {
 	private SaleRepository saleRepository;
 	
 	@Transactional(readOnly = true)
-	public List<Sale> findSales() {
-		return saleRepository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate lastYear = today.minusYears(1);
+	
+		LocalDate min = null;
+		LocalDate max = null;
+		
+		if(minDate.equals("")) {
+			min = lastYear;
+		} else {
+			min = LocalDate.parse(minDate);
+		}
+			
+		if(maxDate.equals("")) {
+			max = today;
+		} else {
+			max = LocalDate.parse(maxDate);
+		}
+		
+		//Maneira alternativa usando ternarios:
+		//LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		//LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return saleRepository.findSales(min, max, pageable);
 	}
 }
